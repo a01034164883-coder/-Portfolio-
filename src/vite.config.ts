0df8f13bee7eecb7b5fa9340b0944ed50eec0667
a01034164883-coder <a01034164883@gmail.com>
2026-06-04 11:24:12 +0900

    Update vite.config.ts

--- src/vite.config.ts
+++ src/vite.config.ts
@@ -1,8 +1,10 @@
-import tailwindcss from '@tailwindcss/vite';
+simport tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
+
 export default defineConfig({
+  root: '.',
   plugins: [react(), tailwindcss()],
   resolve: { alias: { '@': path.resolve(__dirname, '.') } },
 });
